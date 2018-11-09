965ac1153ee5ff37c7aaaf8e59438052136c390b
Samuel Prada <jspradac@gmail.com>
2018-11-09 02:52:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,8 +75,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
   bbq = []
-  holiday_hash.each do |k, v|
-    v.collect do |k, v|
+  holiday_hash.collect do |k, v|
+    v.each do |k, v|
       if v.include?("BBQ")
         bbq.push(k)
       end 
